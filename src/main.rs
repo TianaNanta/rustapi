795f35f1548448a998ee8680e7927bcca568bb8d
TianaNanta <andrianjakananta@gmail.com>
2023-10-16 21:25:00 +0300

    feat: set app name to Actix Web

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
 #[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
+#[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
@@ -18,6 +29,10 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web")
+            }))
+            .service(index)
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
